af840474f87277aec64c93d06f73d3f18a506027
潘常玮 <godv@panchangweideMacBook-Pro.local>
2017-09-02 15:39:47 +0800

    stack

---
+++ Stack/stack.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+#-*- coding:utf-8 -*-
+
+class Stack(object):
+    def __init__(self):
+        self.__items__ = [] 
+
+    def isEmpty(self):
+        return self.__items__ == []
+
+    def push(self,item):
+        self.__items__.append(item)
+
+    def peek(self):
+        return items[-1]
+
+    def pop(self):
+        return self.__items__.pop()
+
+    def size(self):
+        return len(self.__items__)
+
